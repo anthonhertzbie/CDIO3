3b494ec2efc0a0cb52265a7f3bbde3ead7b0afb7
sarabarslund <sara.barslund@outlook.com>
2022-11-04 10:48:11 +0100

    Udkast til Player

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Player {
+    private int playerPosition;
+    String name;
+    private Account acc = new Account();
+
+    public Player (int startMoney, int playerPosition, String navn){
+        this.playerPosition = playerPosition;
+        this.name = navn;
+        acc.setPlayerBalance(startMoney);
+    }
+
+    public void addPlayerPosition(int diceThrow, int money) {
+        this.playerPosition += diceThrow;
+        acc.setPlayerBalance(money);
+    }
+
+    public void setPlayerPosition(int playerPosition, int money) {
+        this.playerPosition = playerPosition;
+        acc.setPlayerBalance(money);
+    }
+
+    public int getAccountBalance() {
+        return acc.getPlayerBalance();
+    }
+    public int getPlayerPosition(){
+        return playerPosition;
+    }
+    public String getName(){
+        return name;
+
+    }
+}
