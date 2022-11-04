ced78a6c734e9af109fcd19c0c17b93325136255
anthonhertzbie <112811601+anthonhertzbie@users.noreply.github.com>
2022-11-04 11:47:33 +0100

    Small corrections

--- src/main/java/org/Game/Cup.java
+++ src/main/java/org/Game/Cup.java
@@ -5,17 +5,9 @@ import java.util.Random;
 import static org.Game.Dice.sides;
 
 public class Cup {
-
     Dice dice1 = new Dice();
     Dice dice2 = new Dice();
-
-    public Cup(){
-    }
-
     private final Random random = new Random();
-
-
-
 public void rollDices() {
     dice1.setFace(random.nextInt(1, sides + 1));
     dice2.setFace(random.nextInt(1, sides + 1));
