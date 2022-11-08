24a3a743b19eea7d8639e1797bfe144edfa6da97
SushiGirrl <lykkeflor@gmail.com>
2022-11-08 17:07:28 +0100

    Restructered Player turn

--- src/main/java/org/Game/GameController.java
+++ src/main/java/org/Game/GameController.java
@@ -1,12 +1,35 @@
 package org.Game;
 
+import java.util.Random;
+
 public class GameController {
 
-    public void startGame(){
-        //Sets starting balance in accordance to number of players
-        int noPlayers = 2;
-        int startMoney = 0;
 
+    Random random = new Random();
+
+    int noPlayers = 2;
+    int startMoney = 0;
+
+    //Random player starts
+    int playerTurn = random.nextInt(0, noPlayers);
+
+    //Instantiates Player class depending on number of players
+    Player[] players = new Player[noPlayers];
+
+
+    //Makes players take turns in the consecutive order 0 through 4
+    public void turn() {
+        if (playerTurn < noPlayers){
+            playerTurn += 1;
+        } else {
+            playerTurn = 0;
+        }
+    }
+
+    //Starts game
+    public void GameController(){
+
+        //Sets starting balance in accordance to number of players
         if (noPlayers == 2){
             startMoney = 1000;
         }
@@ -16,25 +39,28 @@ public class GameController {
         if (noPlayers == 4){
             startMoney = 3000;
         }
-        //Instantiates Player class depending on number of players
-        Player[] players = new Player[noPlayers];
         for (int i = 0; i < noPlayers; i++) {
-            players[i] = new Player(startMoney, 0, "Gui spiller navn");
+            players[i] = new Player(startMoney, 0, "input fra spiller");
         }
-        //Random player starts
-        //Player gets new position
-        //If field type is a property and is unowned, the player buys the property
-        //If field type ia a property and is owned, the player pays rent
-        //If field type is a property and another player owns both, the player pays double rent
-        //If field type is parking the turn ends
-        //If field type is the prison
+
+            //Big ass loop begins
+            //If field type is a property and is unowned, the player buys the property
+            //If field type ia a property and is owned, the player pays rent
+            //If field type is a property and another player owns both, the player pays double rent
+            //If field type is parking the turn ends
+            //If field type is the prison
             //the player can use the jailcard
             //else the player can pay
             //else the player is not allowed to roll until the third round
-        //If field type is chance
+            //If field type is chance
             //chance card is drawn
-                //if chance card is var
-                    //Specific option
+            //if chance card is var
+            //Specific option
+
+        turn(); //skifte-tur-metode
+        players[playerTurn].getName(); //tager fat i specifik spiller
+
+
         //Game ends when a players balance <=0
         //The player with the highest balance wins
     }
