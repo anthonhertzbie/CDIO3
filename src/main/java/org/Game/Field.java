160de5bf3b75cad70c122b0bfe175f56a4a53ecb
CrazyCows <108196449+CrazyCows@users.noreply.github.com>
2022-11-11 02:31:48 +0100

    Reorganized gameboard and field så they follow a propper structure.

--- src/main/java/org/Game/Field.java
+++ src/main/java/org/Game/Field.java
@@ -1,20 +1,39 @@
 package org.Game;
 
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+
+import java.awt.*;
+import java.util.Arrays;
+
 public class Field {
-    private int fieldValue;
 
-    private String fieldName;
 
-    public Field(int fieldValue, String fieldName){
-        this.fieldValue = fieldValue;
-        this.fieldName = fieldName;
-    }
+    String[] fieldInformation = new String[6];
+    GUI_Street field;
+    Helper helper = new Helper();
+    Color color;
+    Color color2;
 
-    public int getFieldValue() {
-        return fieldValue;
-    }
 
-    public void setFieldValue(int fieldValue) {
-        this.fieldValue = fieldValue;
+
+    public GUI_Street getField(String language, int lineNo) {
+        fieldInformation = helper.lineReader(language, lineNo).split("/");
+        // Stolen from https://stackoverflow.com/questions/2854043/converting-a-string-to-color-in-java, Erick Robertson && ZZ Coder
+        try {
+            java.lang.reflect.Field colorReflect = Class.forName("java.awt.Color").getField(fieldInformation[4]);
+            java.lang.reflect.Field colorReflect2 = Class.forName("java.awt.Color").getField(fieldInformation[5]);
+            color = (Color)colorReflect.get(null);
+            color2 = (Color)colorReflect2.get(null);
+        } catch (Exception e) {
+            color = null; // Not defined
+            color2 = null;
+        }
+        // Stealing stops
+
+        System.out.println(fieldInformation[5]);
+        System.out.println(fieldInformation[0] + " || "+ fieldInformation[1]+ " || "+ fieldInformation[2]+ " || "+ fieldInformation[3]+ " || "+ fieldInformation[4]+ " || "+ fieldInformation[5]);
+        this.field = new GUI_Street(fieldInformation[0], fieldInformation[1], fieldInformation[2], fieldInformation[3], color, color2);
+        return field;
     }
 }
--- src/main/java/org/Game/GameBoard.java
+++ src/main/java/org/Game/GameBoard.java
@@ -3,42 +3,20 @@ package org.Game;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
 
-import java.awt.*;
-
 public class GameBoard {
     public GUI_Field[] field2 = new GUI_Field[24];
-    public GUI_Street[] field = new GUI_Street[24];
+    public GUI_Street[] fields = new GUI_Street[24];
+    Field field = new Field();
 
 
     public GUI_Street getField(int currentField) {
-        return field[currentField];
+        return fields[currentField];
     }
 
     public GUI_Street[] createGameBoard(){
-        field[0] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[1] = new GUI_Street("Start", "", "", "-1", Color.WHITE, Color.BLACK);
-        field[2] = new GUI_Street("Start", "", "", "-1", Color.WHITE, Color.BLACK);
-        field[3] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[4] = new GUI_Street("Start", "", "", "-1", Color.WHITE, Color.BLACK);
-        field[5] = new GUI_Street("Start", "", "", "-1", Color.WHITE, Color.BLACK);
-        field[6] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[7] = new GUI_Street("Start", "", "", "-2", Color.WHITE, Color.BLACK);
-        field[8] = new GUI_Street("Start", "", "", "-2", Color.WHITE, Color.BLACK);
-        field[9] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[10] = new GUI_Street("Start", "", "", "-2", Color.WHITE, Color.BLACK);
-        field[11] = new GUI_Street("Start", "", "", "-2", Color.WHITE, Color.BLACK);
-        field[12] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[13] = new GUI_Street("Start", "", "", "-3", Color.WHITE, Color.BLACK);
-        field[14] = new GUI_Street("Start", "", "", "-3", Color.WHITE, Color.BLACK);
-        field[15] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[16] = new GUI_Street("Start", "", "", "-3", Color.WHITE, Color.BLACK);
-        field[17] = new GUI_Street("Start", "", "", "-3", Color.WHITE, Color.BLACK);
-        field[18] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[19] = new GUI_Street("Start", "", "", "-4", Color.WHITE, Color.BLACK);
-        field[20] = new GUI_Street("Start", "", "", "-4", Color.WHITE, Color.BLACK);
-        field[21] = new GUI_Street("Start", "", "", "0", Color.WHITE, Color.BLACK);
-        field[22] = new GUI_Street("Start", "", "", "-5", Color.WHITE, Color.BLACK);
-        field[23] = new GUI_Street("Start", "", "", "-5", Color.WHITE, Color.BLACK);
-        return field;
+        for (int i = 0; i < 24; i++){
+            fields[i] = field.getField("_Fields", i);
+        }
+        return fields;
     }
 }
