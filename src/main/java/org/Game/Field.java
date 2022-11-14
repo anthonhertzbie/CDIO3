32c66eb5e512b864810a2121e2056233de409f0e
CrazyCows <emil.s.simonsen@gmail.com>
2022-11-14 16:06:22 +0100

    Color changes

--- src/main/java/org/Game/Field.java
+++ src/main/java/org/Game/Field.java
@@ -1,11 +1,7 @@
 package org.Game;
-
-import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
 
 import java.awt.*;
-import java.util.Arrays;
-import java.util.SortedMap;
 
 public class Field {
 
@@ -14,23 +10,18 @@ public class Field {
     GUI_Street field;
     Helper helper = new Helper();
     Color color;
-    Color color2;
 
     int R1, G1, B1;
-    int R2, G2, B2;
 
 
     public GUI_Street getField(String language, int lineNo) {
         fieldInformation = helper.lineReader(language, lineNo).split("/");
         // Stolen from https://stackoverflow.com/questions/2854043/converting-a-string-to-color-in-java, Erick Robertson && ZZ Coder
         try {
-            java.lang.reflect.Field colorReflect = Class.forName("java.awt.Color").getField(fieldInformation[4]);
-            java.lang.reflect.Field colorReflect2 = Class.forName("java.awt.Color").getField(fieldInformation[5]);
+            java.lang.reflect.Field colorReflect = Class.forName("java.awt.Color").getField(fieldInformation[5]);
             color = (Color)colorReflect.get(null);
-            color2 = (Color)colorReflect2.get(null);
         } catch (Exception e) {
             color = null; // Not defined
-            color2 = null;
         }
         // Stealing stops
         R1 = Integer.parseInt(fieldInformation[4].split(",")[0]);
@@ -43,7 +34,7 @@ public class Field {
 
         System.out.println(fieldInformation[5]);
         System.out.println(fieldInformation[0] + " || "+ fieldInformation[1]+ " || "+ fieldInformation[2]+ " || "+ fieldInformation[3]+ " || "+ fieldInformation[4]+ " || "+ fieldInformation[5]);
-        this.field = new GUI_Street(fieldInformation[0], fieldInformation[1], fieldInformation[2], fieldInformation[3], new Color(R1,G1,B1), color2);
+        this.field = new GUI_Street(fieldInformation[0], fieldInformation[1], fieldInformation[2], fieldInformation[3], new Color(R1,G1,B1), color);
         this.field.setTextColor(Color.BLACK);
         return field;
     }
